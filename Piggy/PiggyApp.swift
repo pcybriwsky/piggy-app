e79b98825dff29a80bf541910e71f3c9bb55d994
pcybriwsky <pcybriwsky@gmail.com>
2025-01-03 16:35:46 -0500

    First commit

---
+++ Piggy/PiggyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PiggyApp.swift
+//  Piggy
+//
+//  Created by Pedro on 1/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PiggyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PiggyTests/PiggyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PiggyTests.swift
+//  PiggyTests
+//
+//  Created by Pedro on 1/3/25.
+//
+
+import Testing
+@testable import Piggy
+
+struct PiggyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
